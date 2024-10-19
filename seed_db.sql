ba07b5f98842ea72fe66fb58e1500e77ddceb33f
Anthony Menard-Gill <anthonymenardgill@gmail.com>
2024-10-19 14:21:26 -0400

    create table captions

--- seed_db.sql
+++ seed_db.sql
@@ -1,4 +1,11 @@
 -- Here you can add any sql you want such as queries to create tables, insert data, etc.
 -- The file will only be executed when you change anything in it and will run only once.
 
-DROP TABLE trains;
+CREATE TABLE IF NOT EXISTS captions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        video_id TEXT,
+        caption TEXT,
+        start_time REAL,
+        end_time REAL,
+        FOREIGN KEY(video_id) REFERENCES videos(video_id)
+);
